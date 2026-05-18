f3037a4e3e3431443c08dd9f623d75e9af9304e1
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-05-18 18:46:58 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
-import tkinter as tk
+import tkinter as tk  
 import time
 
+
 def update_time():
-    #現在の時刻取得
+    # 現在の時刻取得
     current_time = time.strftime("%H: %M: %S")
     # ラベル
     label.config(text=current_time)
 
     root.after(1000, update_time)
 
-#メインウィンドウ
+
+# メインウィンドウ
 root = tk.Tk()
 root.title("デジタル時計")
 root.geometry("300x150")
@@ -17,8 +19,8 @@ root.geometry("300x150")
 label = tk.Label(root, font=("Helvetica", 48), bg="black", fg="cyan")
 label.pack(expand=True, fill="both")
 
-#時間を更新
+# 時間を更新
 update_time()
 
-#画面を動かし続ける
-root.mainloop() # コレがないと一瞬で消える
+# 画面を動かし続ける
+root.mainloop()  # コレがないと一瞬で消える
