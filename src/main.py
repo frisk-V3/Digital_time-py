1e6057a88ed74dec1acbc4126672e94d045f1e79
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-05-18 18:36:28 +0900

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import tikinter as tk
+import time
+
+def update_time():
+    #現在の時刻取得
+    current_time = time.strftime("%H: %M: %S")
+    # ラベル
+    label.config(text=current_time)
+
+    root.after(1000, update_time)
+
+#メインウィンドウ
+root = tk.Tk()
+root.title("デジタル時計")
+root.geometry("300x150")
+
+label = tk.label(root, font=("Helvetica", 48), bg="black", fg="cyan")
+label.pack(expand=True, fill="both")
+
+#時間を更新
+update_time
+
+#画面を動かし続ける
+root.mainloop() # コレがないと一瞬で消える
